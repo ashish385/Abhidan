e14f9ee382bb040cc956e957792dee62dfa4598c
Ashish <ashish@gmail.com>
2022-12-19 19:24:20 +0530

    complete testimonial

--- frontend/src/assets/Demo.jsx
+++ frontend/src/assets/Demo.jsx
@@ -1,13 +1,36 @@
 import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
-import logo from '../assets/AboutImages/about-us-2.jpg'
-import '../components/Style/about.scss'
+// import logo from '../assets/AboutImages/about-us-2.jpg'
+import '../components/Style/about.css'
+// import "swiper/css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 const Demo = () => {
   return (
     <>
       <Navbar />
-      
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={50}
+        slidesPerView={3}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log("slide change")}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        ...
+      </Swiper>
     </>
   );
 }
---
+++ frontend/src/components/About/AboutSlick.jsx
@@ -0,0 +1,117 @@
+import React from "react";
+import Slider from "react-slick";
+import "../Style/about.css";
+import Logo from "../../assets/AboutImages/about_2.png";
+import image2 from "../../assets/AboutImages/about.jpg";
+
+const AboutSlick = () => {
+  const settings = {
+      lazyLoad: true,
+      spaceBetween:50,
+    initialSlide: 3,
+    dots: false,
+    infinite: true,
+    speed: 3500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    cssEase: "linear",
+    autoplaySpeed: 1000,
+  };
+  return (
+    <>
+      <div className="section-2">
+        <div className="headinpPart">
+          <h1 className="slideHeading ">hello dosto</h1>
+        </div>
+        <Slider {...settings} className="sliderAbout">
+          <div className="slideSection">
+            <div class="card main-card">
+              <div class="image-content">
+                <span class="overlay"></span>
+
+                <div class="card-image">
+                  <img src={Logo} alt="" class="card-img" />
+                </div>
+              </div>
+
+              <div class="card-content">
+                <h2 class="name">David Dell</h2>
+                <p class="description">
+                  The lorem text the section that contains header with having
+                  open functionality. Lorem dolor sit amet consectetur
+                  adipisicing elit.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="slideSection">
+            <div className="card main-card">
+              <div className="image-content">
+                <span className="overlay"></span>
+
+                <div className="card-image">
+                  <img src={image2} alt="" className="card-img" />
+                </div>
+              </div>
+
+              <div className="card-content">
+                <h2 className="name">hello vin0od</h2>
+                <p className="description">
+                  Dekh rhe ho na vinood The lorem text the section that contains
+                  header with having open functionality. Lorem dolor sit amet
+                  consectetur adipisicing elit.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="slideSection">
+            <div class="card main-card">
+              <div class="image-content">
+                <span class="overlay"></span>
+
+                <div class="card-image">
+                  <img src={Logo} alt="" class="card-img" />
+                </div>
+              </div>
+
+              <div class="card-content">
+                <h2 class="name">David Dell</h2>
+                <p class="description">
+                  The lorem text the section that contains header with having
+                  open functionality. Lorem dolor sit amet consectetur
+                  adipisicing elit.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="slideSection">
+            <div className="card main-card">
+              <div className="image-content">
+                <span className="overlay"></span>
+
+                <div className="card-image">
+                  <img src={image2} alt="" className="card-img" />
+                </div>
+              </div>
+
+              <div className="card-content">
+                <h2 className="name">hello vin0od</h2>
+                <p className="description">
+                  Dekh rhe ho na vinood The lorem text the section that contains
+                  header with having open functionality. Lorem dolor sit amet
+                  consectetur adipisicing elit.
+                </p>
+              </div>
+            </div>
+          </div>
+        </Slider>
+      </div>
+    </>
+  );
+};
+
+export default AboutSlick;
