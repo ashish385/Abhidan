53f777d5970006258d31af3c524ce8eb018c3aa9
ayush9936ru <ayush9936ru@gmail.com>
2023-01-31 12:17:25 +0530

    logoslider and circle

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -5,54 +5,7 @@ import "../Footer/footer.css";
 const Footer = () => {
   return (
     <>
-      <footer className="footer">
-        <div className="row">
-          <div className="col">
-            <h4>ABHIDAN</h4>
-            <p>
-              ABHIDAN is web-based application for managing the execces of food
-              in resturant,hotel,hostal and wedding. It is intermediate between
-              two peoples and provide fast and secure service. It's build on
-              MREN technology, which more affecting the fast rendering and
-              working. It is not stop at food but its work on extra donation
-              services .
-            </p>
-          </div>
-
-          <div className="col">
-            <h4>Address</h4>
-            <p>IT Chauraha </p>
-            <p>Lucknow</p>
-            <p>226087</p>
-          </div>
-          <div className="col">
-            <h4>QuickLinks</h4>
-            <p>Home</p>
-            <p>Aboutus</p>
-            <p>Blog</p>
-            <p>Contactus</p>
-          </div>
-          <div className="col">
-            <h4>News Letter</h4>
-            <form>
-              <input
-                type="email"
-                placeholder="Enter your email id"
-              
-                required
-              />
-              <button type="submit" className="btn btn-danger">
-                Subscribe
-              </button>
-            </form>
-            <div className="social-icon">
-         
-            </div>
-          </div>
-        </div>
-        <hr/>
-        <p className="copyright">Abhidan @ 2022-All Right Reserved</p>
-      </footer>
+    
     </>
   );
 };
---
+++ frontend/src/components/Home/Circle.jsx
@@ -0,0 +1,227 @@
+import React from "react";
+import "../Home/Home.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import map from "../../assets/circleback.png";
+import sponimg1 from "../../assets/sponsors2.png"
+const Circle = () => {
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2000,
+    cssEase: "linear"
+  };
+
+  return (
+    <>
+      {/* circle code */}
+      <div className="circleContainer" style={{ backgroundColor: "#efefef" }}>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-7 col-md-8 mx-auto">
+              <div className="sectionTitle text-center mb-65">
+                <span className="sectiontitle">Need your help</span>
+                <h2 className="sectionTitle_big">
+                  we are always where others <br />
+                  needs help
+                </h2>
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-lg-4">
+              <div className="circle-1">
+                <div className="circle-inner">
+                  <div className="circle-wrapper">
+                    <div className="circle-content">
+                      <h3>
+                        {" "}
+                        <span className="circle-num">1000,</span>
+                        <span
+                          className="circle-txt"
+                          style={{ color: "#eb9309" }}
+                        >
+                          KG
+                        </span>
+                      </h3>
+                      <span className="circle-para">
+                        {" "}
+                        <p>
+                          We help local nonprofits access the funding, tools,
+                          training.
+                        </p>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div className="circle-2">
+                <div className="circle-inner">
+                  <div className="circle-wrapper">
+                    <div className="circle-content">
+                      <h3>
+                        {" "}
+                        <span className="circle-num">1000,</span>
+                        <span
+                          className="circle-txt"
+                          style={{ color: "#eb9309" }}
+                        >
+                          KG
+                        </span>
+                      </h3>
+                      <span className="circle-para">
+                        {" "}
+                        <p>
+                          We help local nonprofits access the funding, tools,
+                          training.
+                        </p>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div className="circle-3">
+                <div className="circle-inner">
+                  <div className="circle-wrapper">
+                    <div className="circle-content">
+                      <h3>
+                        {" "}
+                        <span className="circle-num">1000,</span>
+                        <span
+                          className="circle-txt"
+                          style={{ color: "#eb9309" }}
+                        >
+                          KG
+                        </span>
+                      </h3>
+                      <span className="circle-para">
+                        {" "}
+                        <p>
+                          We help local nonprofits access the funding, tools,
+                          training.
+                        </p>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row justify-content-center">
+            <div className="col-lg-4">
+              <div className="circle-4">
+                <div className="circle-inner">
+                  <div className="circle-wrapper">
+                    <div className="circle-content">
+                      <h3>
+                        {" "}
+                        <span className="circle-num">1000,</span>
+                        <span
+                          className="circle-txt"
+                          style={{ color: "#eb9309" }}
+                        >
+                          KG
+                        </span>
+                      </h3>
+                      <span className="circle-para">
+                        {" "}
+                        <p>
+                          We help local nonprofits access the funding, tools,
+                          training.
+                        </p>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div className="circle-5">
+                <div className="circle-inner">
+                  <div className="circle-wrapper">
+                    <div className="circle-content">
+                      <h3>
+                        {" "}
+                        <span className="circle-num">1000,</span>
+                        <span
+                          className="circle-txt"
+                          style={{ color: "#eb9309" }}
+                        >
+                          KG
+                        </span>
+                      </h3>
+                      <span className="circle-para">
+                        {" "}
+                        <p>
+                          We help local nonprofits access the funding, tools,
+                          training.
+                        </p>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* sponsers code */}
+      
+      <section className="sponsors" style={{ backgroundColor: "white" }}>
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <div className="spon_title">
+                <h2 className="spon_heading text-uppercase">
+                  "become support partner"
+                </h2>
+              </div>
+            </div>
+          </div>
+
+          
+          <div className="spon_slider">
+            <div className="spon-track">
+              <div className="spon_img">
+                <img src={sponimg1}style={{ height:"100px",width:"200px"}} alt="sponImage" />
+              </div>
+              <div className="spon_img">
+                <img src={sponimg1} style={{ height:"100px",width:"250px"}}alt="sponImage" />
+              </div>
+              <div className="spon_img">
+                <img src={sponimg1}style={{ height:"100px",width:"250px"}} alt="sponImage" />
+              </div>
+              <div className="spon_img">
+                <img src={sponimg1}style={{ height:"100px",width:"250px"}} alt="sponImage" />
+              </div>
+              <div className="spon_img">
+                <img src={sponimg1}style={{ height:"100px",width:"250px"}} alt="sponImage" />
+              </div>
+              <div className="spon_img">
+                <img src={sponimg1}style={{ height:"100px",width:"250px"}} alt="sponImage" />
+              </div>
+              <div className="spon_img">
+                <img src={sponimg1}style={{ height:"100px",width:"250px"}} alt="sponImage" />
+              </div>
+             
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Circle;
