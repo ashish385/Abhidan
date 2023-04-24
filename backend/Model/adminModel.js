eb620faeb53c3384e2ca1cea59d2450d1031bd6a
Navneet <navneetyadav9198977444@gmail.com>
2023-04-24 21:40:26 +0530

    dashboard@navneet

---
+++ backend/Model/adminModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const admin = mongoose.Schema({
+  
+  email: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const Admin = mongoose.model("Admin", admin);
+module.exports = Admin;
