64e39f3bbbf5ec81eaf0a1422b47317c98ec432b
ayush9936ru <ayush9936ru@gmail.com>
2023-05-11 23:00:01 +0530

    .

--- frontend/src/components/Ngo/Ngopage.jsx
+++ frontend/src/components/Ngo/Ngopage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React,{useEffect, useState} from "react";
 
 import "./Ngopage.css"
 import NgoData from "../../DummyData/NGODummyData.json";
@@ -21,22 +21,43 @@ const Ngopage = () => {
       return setgetNgoData(res.data.data);
     });
   }, []);
-  console.log(getNgoData)
 
 
-
- async function ngoData (){
-  axios.get("http://localhost:1300/api/all-ngo")
-  console.log(ngoData)
- }
  
+
+  
+
+
+
+ 
+ 
+
+
+
+
 
   return (
     <>
       
 
 
-        <div className="flex flex-col relative ">
+<div className="row bg-white overflow-x-hidden">
+  <div className=" col-md-6  mt-2  border-2">
+    < div className="ngo_content text-center mt-10 ">
+    <h1 className="text-2xl font-bold mb-2">Empowering Communities Through <br/>Ngo Integration On Abhidan</h1>
+      <p className="text-lg  text-center">NGO integration is an essential aspect of social development initiatives. It allows for effective utilization of resources, enhances transparency and accountability, and promotes better coordination. By partnering with NGOs, social development projects can achieve a wider reach and create a sustainable impact.</p>
+    </div>
+   
+  </div>
+  <div className="col-md-6">
+  <div className="columns-3xs px-2 mt-2">
+ <img src={ngo_hero}  className="w-full  aspect-video object-fill rounded-lg border-solid border-2 h-48 " alt="ngo"/>
+ <img src={ngo_hero} className="w-full object-contain rounded-lg border-solid border-2 aspect-square mt-2" alt="ngo"/>
+ <img src={ngo_hero} className="w-full  object-contain rounded-lg border-solid border-2 aspect-square  " alt="ngo"/>
+ <img src={ngo_hero} className="w-full  aspect-video object-fill rounded-lg border-solid border-2 h-48 mt-2" alt="ngo"/> 
+ </div>
+  </div>
+        {/* <div className="flex flex-col relative ">
           {NgoData.map((data) => {
             return (
               <div
@@ -60,31 +81,28 @@ const Ngopage = () => {
                     </div>
                     <div className="col-md-8">
                       <h3 className="ngo-name mt-4" style={ngoName}>
-                        {data.NGO}
+                        {data.ngo_name}
                       </h3>
                       <ul className="ngo-det" style={{ color: "#666666" }}>
-                        <li>{data.location}</li>
+                        <li>{data.address}</li>
                         <li>{data.contact}</li>
                       </ul>
-                      <p style={ngoP}>{data.description}</p>
+                      <p style={ngoP}>{data.description}</p> */}
 
-                      <button
-                        type="submit"
-                        className="btn btn-danger mb-4"
-                        // onClick={btnClick}
-                        onClick={() => setShowModal(true)}
-                      >
-                        Donate
-                      </button>
-
-                      {showModal && <Modal setShowModal={setShowModal} />}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-        </div>
+  <div className="row-next mt-2">
+    <div className="search-box ">
+      <input type="search"  placeholder="type here..."className="rounded-full w-[50%]"/>
+    </div>
+    <div className="ngo-card">
+      <div className="ngo-image">
+        <img src={ngo_hero} alt="NGO Logo" />
+      </div>
+      <div className="ngo-details">
+        <p>Organization Name:</p>
+        <p>Register_Id:</p>
+        <p>Contact:</p>
+        <p>Address:</p>
+        <button type="submit" className=" px-3 py-1 text-lg rounded-md border-2 border-yellow-400   hover:bg-yellow-400 "> Donate</button>
       </div>
     </div>
 
