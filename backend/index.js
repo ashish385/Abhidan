1ec81c1cd9037b18c4f308061901811783cb074f
ayush9936 <ayush9936ru@gmail.com>
2023-05-21 12:29:10 +0530

    host and pwa making

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,8 @@
 const express = require("express");
-const db=require("./db");
+const db = require("./db");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
 
 const apiRouter = require("./Routes/apiRoute");
 const abhidanRoutes = require("./Routes/AdminRoutes");
@@ -12,19 +11,17 @@ app.use("/assets", express.static("assets"));
 app.set("view engine", "ejs");
 app.set("views", "./View");
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(express.json());
 
-
-
-
 app.use(
-  cors()
+  cors({
+    origin: ["http://localhost:3000", "https://abhidan-sewa.onrender.com"],
+  })
 );
 
-app.use("/",abhidanRoutes)
+app.use("/", abhidanRoutes);
 app.use("/api", apiRouter);
-
 
 const port = 1300;
 app.listen(port, console.log(`Server started on port no ${port}`));
